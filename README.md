d826a38bdfa345de830bbae223d1d8af949436f6
Simon Wex <simon@simonwex.com>
2020-09-14 17:35:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gatsby Theme Parliament Platform Template
+
+This is a Gatsby site template built with [Gatsby Theme Parliament](https://github.com/adobe/gatsby-theme-parliament).
+
+View the [demo](https://adobedocs.github.io/gatsby-theme-parliament-platform/) running on Github Pages.  
+
+Follow the [instructions](https://github.com/adobe/gatsby-theme-parliament#getting-started) to get started.
---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+const globalNav = require('@adobe/gatsby-theme-parliament/globalNav.json');
+
+globalNav.menus = [globalNav.menus[0]];
+
+module.exports = {
+  siteMetadata: {
+    globalNav,
+    pages: [
+      {
+        title: 'Adobe Creative Cloud',
+        path: '/'
+      }
+    ]
+  },
+  plugins: [`@adobe/gatsby-theme-parliament`],
+  pathPrefix: process.env.PATH_PREFIX || '/gatsby-theme-parliament-platform'
+};
