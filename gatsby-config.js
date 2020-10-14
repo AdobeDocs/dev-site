6ef37999330619de01a2474973f2920b1941dc42
Benge <david.benge@gmail.com>
2020-10-14 00:44:03 -0700

    Update gatsby-config.js
    updated the pathPrefix to root

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,5 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-parliament`],
-  pathPrefix: process.env.PATH_PREFIX || '/dev-site'
+  pathPrefix: process.env.PATH_PREFIX || '/'
 };
