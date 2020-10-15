1e5efa5ff348747279b3db2760c2c35a0b0a8def
Stephan Ringel <ringel@adobe.com>
2020-10-15 09:51:41 +0200

    Fix dev env and update cc links

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,33 @@
-const merge = require("webpack-merge")
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  function getOutput() {
-    switch (stage) {
-      case `develop`:
-        return {
-          filename: `dev-site-assets/[id].js`,
-        }
-      case `build-javascript`:
-        return {
-          filename: `dev-site-assets/[contenthash].js`,
-          chunkFilename: `dev-site-assets/[contenthash].js`,
-        }
+const merge = require('webpack-merge');
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV === 'production') {
+  exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    function getOutput() {
+      switch (stage) {
+        case `develop`:
+          return {
+            filename: `dev-site-assets/[id].js`,
+          }
+        case `build-javascript`:
+          return {
+            filename: `dev-site-assets/[contenthash].js`,
+            chunkFilename: `dev-site-assets/[contenthash].js`,
+          }
+      }
     }
-  }
-  actions.replaceWebpackConfig(merge(getConfig(), { output: getOutput() }))
+    actions.replaceWebpackConfig(merge(getConfig(), { output: getOutput() }))
+  };
 }
