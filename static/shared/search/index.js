ba650c882b5a6599f2f7949ae58adec46bc4a13a
Stephan Ringel <ringel@adobe.com>
2021-08-24 15:00:07 +0200

    Add shared search index

---
+++ static/shared/search/index.js
@@ -0,0 +1,22 @@
+window.AIO_ALGOLIA_INDEX_ALL = [
+  "creative-cloud",
+  "photoshop",
+  "xd",
+  "adobeio",
+  "project-firefly",
+  "stock",
+  "lightroom",
+  "lightroom-classic",
+  "indesign",
+  "illustrator",
+  "premiere-pro",
+  "after-effects",
+  "bridge",
+  "animate",
+  "audition",
+  "dreamweaver",
+  "adobe-fonts",
+  "camera-raw",
+  "customer-journey-analytics",
+  "xmp"
+];
